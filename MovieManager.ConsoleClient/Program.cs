026846b7212d7841316df1d5adbe45c7bf91d3f3
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-04-24 21:49:48 +0200

    Kategorien auslesen.

---
+++ MovieManager.ConsoleClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+
+namespace MovieManager.ConsoleClient
+{
+    class Program
+    {
+        private const string ServerNameWithPort = "localhost:44307";
+
+        static void Main(string[] args)
+        {
+            RetrieveCategories();
+        }
+
+        public static void RetrieveCategories()
+        {
+            var client = new RestClient($"https://{ServerNameWithPort}");
+            var request = new RestRequest("api/categories", DataFormat.Json);
+
+            var response = client.Get(request);
+
+
+            JArray categories = JArray.Parse(response.Content);
+
+            foreach (var category in categories)
+            {
+                Console.WriteLine(category);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
