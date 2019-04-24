27978ca460b777ffdc33b77f6015d0f595842f79
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-04-24 20:27:44 +0200

    GetCategories hinzufügen.

---
+++ MovieManager.Web/ApiControllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManager.Core.Contracts;
+using MovieManager.Persistence;
+using System.Linq;
+
+namespace MovieManager.Web.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        public string[] GetCategories()
+        {
+            using (IUnitOfWork unitOfWork = new UnitOfWork())
+            {
+                return unitOfWork
+                    .CategoryRepository
+                    .GetAll()
+                    .Select(category => category.CategoryName)
+                    .ToArray();
+            }
+        }
+    }
+}
